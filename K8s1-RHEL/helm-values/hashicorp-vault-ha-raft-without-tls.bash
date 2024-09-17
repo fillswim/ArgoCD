6800d88c38fa9d3f5a9e532f4006954b50598ab4
Oleg Filatov <fillswim@gmail.com>
2024-09-17 20:06:53 +0300

    Update hashicorp vault values

--- K8s1-RHEL/helm-values/hashicorp-vault-ha-raft-without-tls.bash
+++
@@ -1,5 +0,0 @@
-# Raft без TLS
-helm install vault fillswimrepo-local/vault \
-    --namespace vault \
-    --create-namespace \
-    -f /root/Kubeadm/Hashicorp-Vault/vault/values-ha-raft-without-tls.yaml
